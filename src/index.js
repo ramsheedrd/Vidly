b964cd14ed3ecdea3dcbf3a190b123d0db287d03
ramsheedrd <ramsheedkc10@gmail.com>
2021-01-12 18:03:28 +0530

    Added Movies Component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'halfmoon/css/halfmoon/css';
+import 'halfmoon/css/halfmoon.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
